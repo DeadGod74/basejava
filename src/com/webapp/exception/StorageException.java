d901b6bfc1f8752540bf94cbda72590c3b1d326c
deadgod <eat-me-drink-me@yandex.ru>
2025-02-27 15:19:16 +0300

    bugFix

--- src/com/webapp/exception/StorageException.java
+++ src/com/webapp/exception/StorageException.java
@@ -8,7 +8,7 @@ public class StorageException extends RuntimeException {
         this.uuid = uuid;
     }
 
-    public StorageException(String uuid) {
-        this.uuid = uuid;
+    public String getUuid() {
+        return uuid;
     }
 }
