e9ab55831e91ff1685944db50e4ff82efd47a5f5
deadgod <eat-me-drink-me@yandex.ru>
2025-03-04 13:16:05 +0300

    hw4

---
+++ src/com/webapp/MainReflection.java
@@ -0,0 +1,22 @@
+package src.com.webapp;
+
+import src.com.webapp.model.Resume;
+
+import java.lang.reflect.Field;
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+
+public class MainReflection {
+    public static void main(String[] args) throws IllegalAccessException, NoSuchMethodException, InvocationTargetException {
+        Resume r = new Resume();
+        Field[] fields = r.getClass().getDeclaredFields();
+        fields[0].setAccessible(true);
+        System.out.println(fields[0].getName());
+        System.out.println(r);
+        fields[0].set(r, "new");
+        System.out.println(r);
+        Method toStringMethod = r.getClass().getMethod("toString");
+        String result = toStringMethod.invoke(r).toString();
+        System.out.println("Result of toString() " + result);
+    }
+}
