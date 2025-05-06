492253ac4dd7e6c2b2349c9d6c53e32a13bbd020
deadgod <eat-me-drink-me@yandex.ru>
2025-05-06 18:37:00 +0300

    add DeadlockExample fix

--- src/com/webapp/util/DeadlockExample.java
+++ src/com/webapp/util/DeadlockExample.java
@@ -5,36 +5,8 @@ public class DeadlockExample {
     private static final Object lock2 = new Object();
 
     public static void main(String[] args) {
-        Thread thread1 = new Thread(() -> {
-            synchronized (lock1) {
-                System.out.println("Thread 1: Holding lock 1");
-                try {
-                    Thread.sleep(200);
-                } catch (InterruptedException e) {
-                    throw new RuntimeException(e);
-                }
-                System.out.println("Thread 1: Waiting lock 2");
-                synchronized (lock2) {
-                    System.out.println("Thread 1: Lock 2 received");
-                }
-            }
-
-        });
-
-        Thread thread2 = new Thread(() -> {
-            synchronized (lock2) {
-                System.out.println("Thread 2: Holding lock 2");
-                try {
-                    Thread.sleep(200);
-                } catch (InterruptedException e) {
-                    throw new RuntimeException(e);
-                }
-                System.out.println("Thread 2: Waiting lock 1");
-                synchronized (lock2) {
-                    System.out.println("Thread 2: Lock 1 received");
-                }
-            }
-        });
+        Thread thread1 = new Thread(new LockingTask(lock1, lock2, "Thread 1"));
+        Thread thread2 = new Thread(new LockingTask(lock2, lock1, "Thread 2"));
 
         thread1.start();
         thread2.start();
@@ -45,6 +17,33 @@ public class DeadlockExample {
         } catch (InterruptedException e) {
             throw new RuntimeException(e);
         }
+    }
 
+    static class LockingTask implements Runnable {
+        private final Object firstLock;
+        private final Object secondlock;
+        private final String name;
+
+        public LockingTask(Object firstLock, Object secondlock, String name) {
+            this.firstLock = firstLock;
+            this.secondlock = secondlock;
+            this.name = name;
+        }
+
+        @Override
+        public void run() {
+            synchronized (firstLock) {
+                System.out.println(name + ":Holding " + firstLock);
+                try {
+                    Thread.sleep(200);
+                } catch (InterruptedException e) {
+                    throw new RuntimeException(e);
+                }
+                System.out.println(name + ":Waiting for " + secondlock);
+                synchronized (secondlock) {
+                    System.out.println(name + ": " + secondlock + "received");
+                }
+            }
+        }
     }
 }
